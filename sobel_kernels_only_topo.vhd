07a16facf27570e7c1d70d46c7ff8a76db55dbe0
ViniciusMartinsFaria <81265270+ViniciusMartinsFaria@users.noreply.github.com>
2025-12-12 22:28:43 -0300

    Update sobel_kernels_only_topo.vhd

--- sobel_kernels_only_topo.vhd
+++ sobel_kernels_only_topo.vhd
@@ -7,13 +7,23 @@ entity sobel_topo is
     port (
         clock               : in std_logic;
         reset               : in std_logic;
-        bloco               : in matriz;
+        entrada_1           : in std_logic_vector(31 downto 0);
+        entrada_2           : in std_logic_vector(31 downto 0);
+        entrada_3           : in std_logic_vector(31 downto 0);
         Gx_saida            : out std_logic_vector(15 downto 0);
         Gy_saida            : out std_logic_vector(15 downto 0) 
     );
 end sobel_topo;
 
 architecture RTL of sobel_topo is
+
+    component interface_bloco is
+    port(
+        pin_1               : in std_logic_vector(31 downto 0);
+        pin_2               : in std_logic_vector(31 downto 0);
+        pin_3               : in std_logic_vector(31 downto 0);
+        bloco               : out matriz);
+    end component;
 
     component G_x is
     port(
@@ -27,20 +37,29 @@ architecture RTL of sobel_topo is
         saida       : out std_logic_vector(15 downto 0));
     end component;
 
+    signal bloco_sg     : matriz;
     signal Gx_sg        : std_logic_vector(15 downto 0);
     signal Gy_sg        : std_logic_vector(15 downto 0);
 
 begin
 
+    inst_interface_bloco: interface_bloco
+    port map (
+        pin_1   => entrada_1,
+        pin_2   => entrada_2,
+        pin_3   => entrada_3,
+        bloco   => bloco_sg
+    );
+
     inst_Gx: G_x
     port map (
-        entrada => bloco,
+        entrada => bloco_sg,
         saida   => Gx_sg
     );
 
     inst_Gy: G_y
     port map (
-        entrada => bloco,
+        entrada => bloco_sg,
         saida   => Gy_sg
     );
     
