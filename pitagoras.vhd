fff838d472ddb8aaea4adcf39aa6bcc636ee2219
ViniciusMartinsFaria <81265270+ViniciusMartinsFaria@users.noreply.github.com>
2025-12-12 22:27:32 -0300

    Update pitagoras.vhd

--- pitagoras.vhd
+++ pitagoras.vhd
@@ -7,16 +7,19 @@ entity pitagoras is
     port(
         Gx          : in  std_logic_vector(15 downto 0);
         Gy          : in  std_logic_vector(15 downto 0);
-        saida       : out std_logic_vector(15 downto 0)
+        saida       : out std_logic_vector(7 downto 0)
         );
 end pitagoras;
 
 architecture RTL of pitagoras is
 
+    signal clamping :std_logic_vector(15 downto 0);
 
 begin
 
     -- Soma aproximada de magnitude(Vinícius)
-    saida <= std_logic_vector(abs(signed(Gx)) + abs(signed(Gy)));
+    clamping    <= std_logic_vector(abs(signed(Gx)) + abs(signed(Gy)));
+    saida       <= 	"11111111" when clamping(15 downto 8) /= "00000000" else
+				    clamping(7 downto 0);
 
 end RTL;
